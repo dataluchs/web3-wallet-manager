452a3ddeb33ecdb04e86eef1ce423283b77ebbd0
dataluchs <tim_stehle@posteo.de>
2021-08-24 23:01:47 +0200

    update navbar for logout

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
-import Link from 'next/link'
+import Link from "next/link";
 import { useMoralis } from "react-moralis";
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
-
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon, LogoutIcon } from "@heroicons/react/solid";
+import { toast } from "react-toastify";
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+  return classes.filter(Boolean).join(" ");
+}
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
   const { logout, isAuthenticated, user } = useMoralis();
-
 
   return (
     <div>
@@ -23,27 +22,26 @@ function Nav() {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center w-full justify-between">
               <div className="flex-shrink-0">
-              <Link href="/">
+                <Link href="/">
                   <a>
                     <img
-                        className="h-8 w-auto"
-                        //src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        src="/images/blockfriends.png"
-                        alt="blockfriends"
-                        />
-                    </a>
+                      className="h-8 w-auto"
+                      //src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                      src="/images/blockfriends.png"
+                      alt="blockfriends"
+                    />
+                  </a>
                 </Link>
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
-                  
                   <Link href={"/"}>
-                  <a
-                    href="#"
-                    className="hover:bg-white text-black px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Dashboard
-                  </a>
+                    <a
+                      href="#"
+                      className="hover:bg-white text-black px-3 py-2 rounded-md text-sm font-medium"
+                    >
+                      Dashboard
+                    </a>
                   </Link>
                   <a
                     href="/account"
@@ -59,117 +57,94 @@ function Nav() {
                     Projects
                   </a>
 
-                  {/* <Link href={"/auth/login"}>
-                  <a
-                    href="#"
-                    className="hover:bg-blue text-black px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Login
-                  </a>
-                  </Link> */}
+                  {!isAuthenticated ? (
+                    <Link href={"/auth"}>
+                      <button className="bg-green-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Connect / Login
+                      </button>
+                    </Link>
+                  ) : !user ? (
+                    <div>
+                      <p className="bg-gray-200 border-2 hover:border-blue-400 text-black px-3 py-2 rounded-md text-sm font-medium">
+                        {" "}
+                        loading ..
+                      </p>
+                    </div>
+                  ) : (
+                    <>
+                      <Menu
+                        as="div"
+                        className="relative inline-block text-left"
+                      >
+                        <div>
+                          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-purple-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                            {user.get("username")}
+                            <ChevronDownIcon
+                              className="-mr-1 ml-2 h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          </Menu.Button>
+                        </div>
 
-          
-                        
-                    {
-                        !isAuthenticated ? (
-                            <Link href={"/auth/login"}>
-                                <a
-                                    href="#"
-                                    className="bg-purple-300 hover:bg-blue text-black px-3 py-2 rounded-md text-sm font-medium"
-                                >
-                                    Login
-                                </a>
-                            </Link>
-                        ) : (
-                            !user ? 
-                            
-                            <div>
-                                <p
-                                    className="bg-gray-200 border-2 hover:border-blue-400 text-black px-3 py-2 rounded-md text-sm font-medium"
-                                    > loading ..
-                                </p>
-                            </div> 
-                            
-                            : 
-                            <>
-                            
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="/account"
+                                    className={classNames(
+                                      active
+                                        ? "bg-gray-100 text-gray-900"
+                                        : "text-gray-700",
+                                      "block px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    Account settings
+                                  </a>
+                                )}
+                              </Menu.Item>
 
-                            <Menu as="div" className="relative inline-block text-left">
-                                <div>
-                                    <Menu.Button className="inline-flex justify-center w-full rounded-md border border-purple-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                                    {user.get("username")}
-                                    <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-                                    </Menu.Button>
-                                </div>
-
-                                <Transition
-                                    as={Fragment}
-                                    enter="transition ease-out duration-100"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                >
-                                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <div className="py-1">
-                                        <Menu.Item>
-                                        {({ active }) => (
-                                            <a
-                                            href="/account"
-                                            className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                            )}
-                                            >
-                                            Account settings
-                                            </a>
-                                        )}
-                                        </Menu.Item>
-                                        
-                                        <form method="POST" action="#">
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                            <button
-                                                type="submit"
-                                                onClick={() => logout()}
-                                                className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'block w-full text-left px-4 py-2 text-sm'
-                                                )}
-                                            >
-                                                Sign out
-                                            </button>
-                                            )}
-                                        </Menu.Item>
-                                        </form>
-                                    </div>
-                                    </Menu.Items>
-                                </Transition>
-                                </Menu>
-                            
-
-
-
-                            {/* <div>
-                                <p
-                                    className="bg-gray-200 border-2 hover:border-blue-400 text-black px-3 py-2 rounded-md text-sm font-medium"
-                                    >{user.get("username")}
-                                </p>
+                              <form>
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <button
+                                      type="submit"
+                                      onClick={() => logout()}
+                                      onClick={() =>
+                                        logout({
+                                          onSuccess: () =>
+                                            toast.success(
+                                              "Successfully signed out. See you soon 😘"
+                                            ),
+                                        })
+                                      }
+                                      className={classNames(
+                                        active
+                                          ? "bg-gray-100 text-gray-900"
+                                          : "text-gray-700",
+                                        "block w-full text-left px-4 py-2 text-sm"
+                                      )}
+                                    >
+                                      Sign out
+                                    </button>
+                                  )}
+                                </Menu.Item>
+                              </form>
                             </div>
-
-                            <button
-                                className="hover:bg-blue text-black px-3 py-2 rounded-md text-sm font-medium"
-                                onClick={() => logout()}
-                            >
-                                Logout
-                            </button> */}
-                            </>
-                            
-                        )
-                    
-                    } 
- 
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
@@ -229,7 +204,7 @@ function Nav() {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          {(ref) => (
+          {ref => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <a
